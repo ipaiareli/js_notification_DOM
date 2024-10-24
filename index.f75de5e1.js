c787f685793d5958602234a588e16e946c4e3d36
Ivan Paiareli <ipayareli@gmail.com>
2024-10-24 21:37:19 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,25 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const body = document.querySelector("body");
+    const elem = document.createElement("div");
+    const elemTitle = document.createElement("h2");
+    const elemDescription = document.createElement("p");
+    body.appendChild(elem);
+    elem.appendChild(elemTitle);
+    elem.appendChild(elemDescription);
+    elem.classList.add("notification");
+    elem.classList.add(type);
+    elemTitle.classList.add("title");
+    elemTitle.textContent = title;
+    elemDescription.textContent = description;
+    elem.style.top = `${posTop}px`;
+    elem.style.right = `${posRight}px`;
+    setTimeout(()=>{
+        elem.style.visibility = "hidden";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
